444b75dad6d3bb9b0d11a5a4f505cc067c3a691b
BOUZIDI Mohamed Ali <bouzidi.medali20@gmail.com>
2024-05-21 18:58:35 +0200

    Fixed task 7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,7 +6,9 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """Basic Auth class"""
 
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(
+        self, authorization_header: str
+    ) -> str:
         """Extract the base64 authorization header"""
         if (
             authorization_header is None
