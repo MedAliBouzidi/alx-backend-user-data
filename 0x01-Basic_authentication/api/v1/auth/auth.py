87bd87230ddae555403948f175bb11eaa4256d25
BOUZIDI Mohamed Ali <bouzidi.medali20@gmail.com>
2024-05-23 19:28:20 +0200

    task 13

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """ Auth module for the API """
+import re
 from typing import List, TypeVar
 
 from flask import request
@@ -17,10 +18,10 @@ class Auth:
         """
         if path is None or excluded_paths is None or excluded_paths == []:
             return True
-        if path[-1] != "/":
-            path += "/"
-        if path in excluded_paths:
-            return False
+        for p in excluded_paths:
+            if re.match(p, path):
+                return False
+
         return True
 
     def authorization_header(self, request=None) -> str:
