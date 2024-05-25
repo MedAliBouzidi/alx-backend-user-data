4119e0c35978d653c45485c656236f580caeefb9
BOUZIDI Mohamed Ali <bouzidi.medali20@gmail.com>
2024-05-25 19:43:48 +0200

    Fixed pycodestyle for task 2

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -9,7 +9,8 @@ class SessionAuth(Auth):
     """ Session Authentication Class"""
 
     user_id_by_session_id = {}
-    def create_session(self, user_id: str = None)-> str:
+
+    def create_session(self, user_id: str = None) -> str:
         """ Create a session ID """
         if user_id is None or not isinstance(user_id, str):
             return None
