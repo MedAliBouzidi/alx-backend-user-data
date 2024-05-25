626e228d11d398c0c0eb51f5dfb990e79ef26ca0
BOUZIDI Mohamed Ali <bouzidi.medali20@gmail.com>
2024-05-25 19:21:08 +0200

    task 0

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """ Module of Users views
 """
-from api.v1.views import app_views
 from flask import abort, jsonify, request
+
+from api.v1.views import app_views
 from models.user import User
 
 
@@ -27,6 +28,10 @@ def view_one_user(user_id: str = None) -> str:
     """
     if user_id is None:
         abort(404)
+    if user_id == 'me':
+        if request.current_user is None:
+            abort(404)
+        return jsonify(request.current_user.to_json())
     user = User.get(user_id)
     if user is None:
         abort(404)
