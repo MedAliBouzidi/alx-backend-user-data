58f5c1075c5ae23f5d763c460ba32e8ddd7059c8
BOUZIDI Mohamed Ali <bouzidi.medali20@gmail.com>
2024-05-26 16:24:25 +0200

    task 7

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+""" Session Authentication View """
+from os import getenv
+
+from flask import jsonify, request
+
+from api.v1.views import app_views
+from models.user import User
+
+
+@app_views.route("/auth_session/login", methods=["POST"], strict_slashes=False)
+def login():
+    """
+    POST /auth_session/login
+        Return:
+          - User object JSON representation
+    """
+    email = request.form.get("email")
+    if email is None or email == "":
+        return {"error": "email missing"}, 400
+    password = request.form.get("password")
+    if password is None or password == "":
+        return {"error": "password missing"}, 400
+    user = User.search({"email": email})
+    user = user[0] if user else None
+    if user is None:
+        return {"error": "no user found for this email"}, 404
+    if not user.is_valid_password(password):
+        return {"error": "wrong password"}, 401
+
+    from api.v1.app import auth
+
+    session_id = auth.create_session(user.id)
+    response = jsonify(user.to_json())
+    response.set_cookie(getenv("SESSION_NAME"), session_id)
+    return response
