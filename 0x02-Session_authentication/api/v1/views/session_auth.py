da0595b0590da0d966af6528e1cbf1010660c56e
BOUZIDI Mohamed Ali <bouzidi.medali20@gmail.com>
2024-05-26 16:36:00 +0200

    task 8

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -2,7 +2,7 @@
 """ Session Authentication View """
 from os import getenv
 
-from flask import jsonify, request
+from flask import abort, jsonify, request
 
 from api.v1.views import app_views
 from models.user import User
@@ -34,3 +34,12 @@ def login():
     response = jsonify(user.to_json())
     response.set_cookie(getenv("SESSION_NAME"), session_id)
     return response
+
+@app_views.route("/auth_session/logout", methods=["DELETE"], strict_slashes=False)
+def logout():
+    """ Logout from Sesson """
+    from api.v1.app import auth
+
+    if not auth.destroy_session(request):
+        abort(404)
+    return jsonify({}), 200
