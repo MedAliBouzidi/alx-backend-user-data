40a36353ebec4ac2e104f63d2a0d8950e0780d52
BOUZIDI Mohamed Ali <bouzidi.medali20@gmail.com>
2024-05-26 18:13:02 +0200

    task 10

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -29,6 +29,10 @@ if auth_type == "session_exp_auth":
     from api.v1.auth.session_exp_auth import SessionExpAuth
 
     auth = SessionExpAuth()
+if auth_type == "session_db_auth":
+    from api.v1.auth.session_db_auth import SessionDBAuth
+
+    auth = SessionDBAuth()
 else:
     from api.v1.auth.auth import Auth
 
