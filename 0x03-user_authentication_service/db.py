7e25acfeb6b4a1e8a32cda268af1b9d224d32b67
BOUZIDI Mohamed Ali <bouzidi.medali20@gmail.com>
2024-06-01 19:30:28 +0200

    fix import

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -3,11 +3,9 @@
 """
 from sqlalchemy import create_engine
 from sqlalchemy.exc import InvalidRequestError
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import query, sessionmaker
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm.session import Session
-from sqlalchemy.sql.operators import endswith_op
 
 from user import Base, User
 
